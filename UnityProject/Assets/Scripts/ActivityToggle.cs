2090c2a84d699c36e11c5f211c3c83c80b2e8b06
Alexander Schallenberg <griszder@gmail.com>
2022-04-23 21:01:55 +0200

    Fixed spelling error Renamed file and class ActivatyToggle to ActivityToggle

--- UnityProject/Assets/Scripts/ActivityToggle.cs
+++ UnityProject/Assets/Scripts/ActivityToggle.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ActivatyToggle : MonoBehaviour {
+public class ActivityToggle : MonoBehaviour {
     public GameObject GameObject;
 
     public void ToggleActivity() {
