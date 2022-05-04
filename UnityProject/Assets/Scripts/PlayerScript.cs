e3788d7c89ce7413ee356ee529784a88b8900c4a
Alexander Schallenberg <griszder@gmail.com>
2022-05-04 14:51:07 +0200

    Implemented Player Added PlayerScript for movement Added capsule as temporary player Subordinated camera to the player
    STATUS:
    Tested!
    Movement is correct
    NullReferenceException produced by Buidling/MousePosition (Building system)

---
+++ UnityProject/Assets/Scripts/PlayerScript.cs
@@ -0,0 +1,35 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Serialization;
+
+public class PlayerScript : MonoBehaviour {
+    public float MovementSpeed = 5f;
+    
+    private GameInputActions.PlayerActions _playerActions;
+    private Vector2 _move = Vector2.zero;
+
+    private void OnEnable() {
+        _playerActions.Enable();
+    }
+
+    private void OnDisable() {
+        _playerActions.Disable();
+    }
+
+    private void Awake() {
+        _playerActions = Util.InputAction.Player;
+
+        _playerActions.Movement.performed += context => _move = context.ReadValue<Vector2>();
+        _playerActions.Movement.canceled += _ => _move = Vector2.zero;
+    }
+
+    private void Update() {
+        if (_move != Vector2.zero) {
+            var velocity = _move * (Time.deltaTime * MovementSpeed);
+            var pos = transform.position;
+            transform.position = new Vector3(pos.x + velocity.x, pos.y + velocity.y, pos.z);
+        }
+    }
+}
