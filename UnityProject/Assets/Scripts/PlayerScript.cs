c1f947f51dd2d0acfa686d010c65385c6936c3b1
Alexander Schallenberg <griszder@gmail.com>
2022-05-05 23:18:39 +0200

    Optimized imports in PlayerScript, StructureHandler, Util

--- UnityProject/Assets/Scripts/PlayerScript.cs
+++ UnityProject/Assets/Scripts/PlayerScript.cs
@@ -1,8 +1,4 @@
-using System;
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.Serialization;
 
 public class PlayerScript : MonoBehaviour {
     public float MovementSpeed = 5f;
