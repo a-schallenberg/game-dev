7534dc337ee0324366b0d0b17ee8ac6b41358fdd
Alexander Schallenberg <griszder@gmail.com>
2022-05-29 11:05:24 +0200

    Removed unused methods and imports

--- UnityProject/Assets/Scripts/Util.cs
+++ UnityProject/Assets/Scripts/Util.cs
@@ -1,26 +1,3 @@
-using Game.Structures;
-using UnityEngine;
 
-public static class Util {
-	public static Structure FindNextStructure(GameObject gameObject) {
-		var structures = gameObject.GetComponents<Structure>();
 
-		if (structures.Length != 0) {
-			return structures[0];
-		}
-
-		structures = gameObject.GetComponentsInChildren<Structure>();
-
-		if (structures.Length != 0) {
-			return structures[0];
-		}
-
-		structures = gameObject.GetComponentsInParent<Structure>();
-
-		if (structures.Length != 0) {
-			return structures[0];
-		}
-
-		return null;
-	}
-}
+public static class Util {}
