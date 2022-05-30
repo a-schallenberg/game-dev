79247a5f0a29507cbdeb226e6c2557a31c1585bf
Alexander Schallenberg <griszder@gmail.com>
2022-05-30 16:11:13 +0200

    Commented Interactable

--- UnityProject/Assets/Scripts/Game/Interactable.cs
+++ UnityProject/Assets/Scripts/Game/Interactable.cs
@@ -2,9 +2,18 @@ using UnityEngine;
 using UnityEngine.Events;
 
 namespace Game {
+	
+	/// <summary>
+	/// Makes <c>GameObject</c> interactable
+	/// </summary>
 	public class Interactable : MonoBehaviour {
 		[SerializeField] private UnityEvent<Collider2D> onInteract;
 
+		/// <summary>
+		/// Has to be run on interacting with this <c>GameObject</c>
+		/// Runs all onInteract actions
+		/// </summary>
+		/// <param name="col">The collider, which is triggered for interacting</param>
 		public virtual void Interact(Collider2D col) {
 			onInteract.Invoke(col);
 		}
