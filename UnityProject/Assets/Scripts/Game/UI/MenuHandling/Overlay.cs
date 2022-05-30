82226054a0544742dbb4890d1d25d38154050f51
Alexander Schallenberg <griszder@gmail.com>
2022-05-30 16:20:00 +0200

    Commented Overlay

--- UnityProject/Assets/Scripts/Game/UI/MenuHandling/Overlay.cs
+++ UnityProject/Assets/Scripts/Game/UI/MenuHandling/Overlay.cs
@@ -1,15 +1,26 @@
 using UnityEngine;
 
 namespace Game.UI.MenuHandling {
+	
+	/// <summary>
+	/// Manages the Overlay
+	/// </summary>
 	public class Overlay : MonoBehaviour {
+		public static            Overlay        Instance { get; private set; }
+		
+		/// <summary>
+		/// <c>ActivityToggle</c> of the overlay
+		/// </summary>
 		[SerializeField] private ActivityToggle activityToggle;
+
 
 		public Overlay() {
 			Instance = this;
 		}
 
-		public static Overlay Instance { get; private set; }
-
+		/// <summary>
+		/// Getter of the <c>ActivityToggle</c> attribute
+		/// </summary>
 		public ActivityToggle ActivityToggle {
 			get { return activityToggle; }
 		}
